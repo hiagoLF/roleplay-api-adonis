967a9313f1db14fb2060b77422da06924f74a389
hiagoLF <hiagolof@gmail.com>
2022-05-04 17:33:38 -0300

    Reset Password Controllers and Testes

---
+++ test/users/paswords.spec.ts
@@ -0,0 +1,124 @@
+import { UserFactory } from 'Database/factories'
+import Database from '@ioc:Adonis/Lucid/Database'
+import test from 'japa'
+import supertest from 'supertest'
+import Mail from '@ioc:Adonis/Addons/Mail'
+import Hash from '@ioc:Adonis/Core/Hash'
+import { DateTime, Duration } from 'luxon'
+
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+
+test.group('Password', (group) => {
+  test('it should send an email with forgot password instructions', async (assert) => {
+    const user = await UserFactory.create()
+
+    Mail.trap((message) => {
+      assert.deepEqual(message.to, [{ address: user.email }])
+      assert.deepEqual(message.from, { address: 'no-reply@roleplay.com' })
+      assert.equal(message.subject, 'Roleplay: Recuperação de Senha')
+      assert.include(message.html!, user.username)
+    })
+
+    await supertest(BASE_URL)
+      .post('/forgot-password')
+      .send({
+        email: user.email,
+        resetPaswordUrl: 'url',
+      })
+      .expect(204)
+
+    Mail.restore()
+  })
+
+  test('it should create a reset password token', async (assert) => {
+    const user = await UserFactory.create()
+
+    Mail.trap(() => {})
+
+    await supertest(BASE_URL)
+      .post('/forgot-password')
+      .send({
+        email: user.email,
+        resetPaswordUrl: 'url',
+      })
+      .expect(204)
+
+    Mail.restore()
+
+    const tokens = await user.related('tokens').query()
+    assert.isNotEmpty(tokens)
+  })
+
+  test('it should return 422 when requires data is not provided or data is invalid', async (assert) => {
+    Mail.trap(() => {})
+
+    const { body } = await supertest(BASE_URL).post('/forgot-password').send({}).expect(422)
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 422)
+
+    Mail.restore()
+  })
+
+  test('it should be able to reset password', async (assert) => {
+    const user = await UserFactory.create()
+    const { token } = await user.related('tokens').create({ token: 'token' })
+    const newPassword = '123456'
+    await supertest(BASE_URL)
+      .post('/reset-password')
+      .send({ token, password: '123456' })
+      .expect(204)
+    await user.refresh()
+    const checkPassword = await Hash.verify(user.password, newPassword)
+    assert.isTrue(checkPassword)
+  })
+
+  test('it should return 422 when requires data is not provided or data is invalid', async (assert) => {
+    Mail.trap(() => {})
+
+    const { body } = await supertest(BASE_URL).post('/reset-password').send({}).expect(422)
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 422)
+
+    Mail.restore()
+  })
+
+  test('it should return 404 when using the same token twice', async (assert) => {
+    const user = await UserFactory.create()
+    const { token } = await user.related('tokens').create({ token: 'token' })
+
+    await supertest(BASE_URL)
+      .post('/reset-password')
+      .send({ token, password: '123456' })
+      .expect(204)
+
+    const { body } = await supertest(BASE_URL)
+      .post('/reset-password')
+      .send({ token, password: '123456' })
+      .expect(404)
+
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 404)
+  })
+
+  test('it cannot reset password when token is expired after 2 hours', async (assert) => {
+    const user = await UserFactory.create()
+    const date = DateTime.now().minus(Duration.fromISOTime('02:01'))
+    const { token } = await user.related('tokens').create({ token: 'token', createdAt: date })
+    const { body } = await supertest(BASE_URL)
+      .post('/reset-password')
+      .send({ token, password: '123456' })
+      .expect(410)
+
+    assert.equal(body.code, 'TOKEN_EXPIRED')
+    assert.equal(body.status, 410)
+    assert.equal(body.message, 'token has expired')
+  })
+
+  group.beforeEach(async () => {
+    await Database.beginGlobalTransaction()
+  })
+
+  group.afterEach(async () => {
+    await Database.rollbackGlobalTransaction()
+  })
+})
